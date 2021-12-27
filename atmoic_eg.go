6d6b3dab820cb092910563e6b9a2db9bdea305ef
ArpitSharma2800 <1arpit2000sharma@gmail.com>
2021-12-27 08:46:47 +0530

    concurrecny | fixed atomic

--- atmoic_eg.go
+++ atmoic_eg.go
@@ -8,9 +8,7 @@ import (
 )
 
 func main() {
-	fmt.Println("GO CPUs", runtime.NumCPU())
-	fmt.Println("GO routines", runtime.NumGoroutine())
-	 var counter int64 //creating atmoic of int64
+	var counter int64 //creating atomic of int64
 	const predator = 100
 	var wg sync.WaitGroup
 	wg.Add(predator) //creating waitgrp
@@ -18,14 +16,12 @@ func main() {
 	for i:=0; i<predator; i++{
 		go func() {
 			atomic.AddInt64(&counter,1) //adding pointer to the counter with value 1
-			runtime.Gosched() //it yields the processor, allowing other goroutines to run
 			fmt.Println("Counter", atomic.LoadInt64(&counter)) //loading pointer to the counter
 			wg.Done()
 		}()
 		fmt.Println("GO routines", runtime.NumGoroutine())
 	}
 	wg.Wait()
-	fmt.Println("GO routines", runtime.NumGoroutine())
 	fmt.Println("GO counter", counter)
 }
 
